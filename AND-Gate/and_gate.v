e9f563e6713b1d62ff456e9fb391191c0279a12c
dharshiniaquila <mdharshini684@gmail.com>
2026-06-10 12:56:51 +0530

    Add files via upload

---
+++ AND-Gate/and_gate.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10.06.2026 12:11:18
+// Design Name: 
+// Module Name: and_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module and_gate(
+    input A,
+    input B,
+    output Y
+);
+
+assign Y = A & B;
+
+endmodule
---
+++ AND-Gate/tb_and_gate.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10.06.2026 12:13:09
+// Design Name: 
+// Module Name: tb_and_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_and_gate;
+reg A, B;
+wire Y;
+
+and_gate uut(
+    .A(A),
+    .B(B),
+    .Y(Y)
+);
+
+initial begin
+    A=0; B=0;
+    #10 A=0; B=1;
+    #10 A=1; B=0;
+    #10 A=1; B=1;
+    
+end
+endmodule
